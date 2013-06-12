18b96442ebda1916e4feee905428a71b12e51294
Marc Fournier <marc.fournier@camptocamp.com>
2013-06-12 10:58:11 +0200

    linted whole module

--- manifests/module.pp
+++ manifests/module.pp
@@ -1,71 +1,77 @@
-/*
-== Definition: selinux::module
-
-This definition builds a binary SELinux module from a supplied set of SELinux
-security policies. It is then possible to load it using a Selmodule resource.
-
-Parameters:
-
-- *name*: the name of the SELinux module
-- *workdir*: where the module source and binary files are stored. Defaults to
-  "/etc/puppet/selinux"
-- *dest*: where the binary module must be copied. Defaults to
-  "/usr/share/selinux/targeted/"
-- *content*: inline content or template of the module source
-- *source*: file:// or puppet:// URI of the module source file
-
-Example usage:
-
-  selinux::module { "foobar":
-    notify => Selmodule["foobar"],
-    source => "puppet:///modules/myproject/foobar.te",
-  }
-
-  selmodule { "foobar":
-    ensure      => present,
-    syncversion => true,
-  }
-
-*/
-define selinux::module ($workdir="/etc/puppet/selinux", $dest="/usr/share/selinux/targeted/", $content=undef, $source=undef) {
+# == Definition: selinux::module
+#
+# This definition builds a binary SELinux module from a supplied set of SELinux
+# security policies. It is then possible to load it using a Selmodule resource.
+#
+# Parameters:
+#
+# - *name*: the name of the SELinux module
+# - *workdir*: where the module source and binary files are stored. Defaults to
+#   "/etc/puppet/selinux"
+# - *dest*: where the binary module must be copied. Defaults to
+#   "/usr/share/selinux/targeted/"
+# - *content*: inline content or template of the module source
+# - *source*: file:// or puppet:// URI of the module source file
+#
+# Example usage:
+#
+#   selinux::module { "foobar":
+#     notify => Selmodule["foobar"],
+#     source => "puppet:///modules/myproject/foobar.te",
+#   }
+#
+#   selmodule { "foobar":
+#     ensure      => present,
+#     syncversion => true,
+#   }
+#
+define selinux::module (
+  $workdir='/etc/puppet/selinux',
+  $dest='/usr/share/selinux/targeted/',
+  $content=undef,
+  $source=undef
+) {
 
   if !defined(File[$workdir]) {
-    file { "$workdir":
+    file { $workdir:
       ensure => directory,
-      mode   => 0700,
-      owner  => "root",
+      mode   => '0700',
+      owner  => 'root',
     }
   }
 
   if $content {
     file { "${workdir}/${name}.te":
-      ensure => present,
+      ensure  => present,
       content => $content,
       require => File[$workdir],
-      notify => Exec["build selinux policy module ${name}"],
+      notify  => Exec["build selinux policy module ${name}"],
     }
   }
   if $source {
     file { "${workdir}/${name}.te":
-      ensure => present,
-      source => $source,
+      ensure  => present,
+      source  => $source,
       require => File[$workdir],
-      notify => Exec["build selinux policy module ${name}"],
+      notify  => Exec["build selinux policy module ${name}"],
     }
   }
 
   exec { "build selinux policy module ${name}":
-    cwd => $workdir,
+    cwd     => $workdir,
     command => "checkmodule -M -m ${name}.te -o ${name}.mod",
     creates => "${workdir}/${name}.mod",
-    require => [File["${workdir}/${name}.te"], Package["checkpolicy"]],
-    notify => Exec["build selinux policy package ${name}"],
+    require => [File["${workdir}/${name}.te"], Package['checkpolicy']],
+    notify  => Exec["build selinux policy package ${name}"],
   }
 
   exec { "build selinux policy package ${name}":
-    cwd => $workdir,
+    cwd     => $workdir,
     command => "semodule_package -o ${dest}/${name}.pp -m ${name}.mod",
     creates => "${dest}/${name}.pp",
-    require => [Exec["build selinux policy module ${name}"], Package["policycoreutils"]],
+    require => [
+      Exec["build selinux policy module ${name}"],
+      Package['policycoreutils'],
+    ],
   }
 }
--- manifests/seport.pp
+++ manifests/seport.pp
@@ -1,44 +1,42 @@
-/*
-== Definition: selinux::seport
-
-Adds/removes ports to SELinux security contexts.
-
-Parameters:
-
-- *$name*: security context name
-- *$ensure*: present/absent
-- *$proto*: tcp/udp
-- *$port*: port number to add/remove from security context
-
-Example usage:
-
-  # allow apache to bind on port 8001
-  selinux::seport { "http_port_t":
-    ensure => present,
-    proto  => "tcp",
-    port   => "8001",
-    before => Service["apache"],
-  }
-
-*/
-define selinux::seport($ensure='present', $proto='tcp', $port) {
+# == Definition: selinux::seport
+#
+# Adds/removes ports to SELinux security contexts.
+#
+# Parameters:
+#
+# - *$name*: security context name
+# - *$ensure*: present/absent
+# - *$proto*: tcp/udp
+# - *$port*: port number to add/remove from security context
+#
+# Example usage:
+#
+#   # allow apache to bind on port 8001
+#   selinux::seport { "http_port_t":
+#     ensure => present,
+#     proto  => "tcp",
+#     port   => "8001",
+#     before => Service["apache"],
+#   }
+#
+define selinux::seport($port, $ensure='present', $proto='tcp') {
 
   # this is dreadful to read, sorry...
 
   $re = "^${name}\W+${proto}\W+.*\W${port}(\W|$)"
 
-  if $ensure == "present" {
-    $semanage = "--add"
-    $grep     = "egrep -q"
+  if $ensure == 'present' {
+    $mgt  = '--add'
+    $grep = 'egrep -q'
   } else {
-    $semanage = "--delete"
-    $grep     = "! egrep -q"
+    $mgt  = '--delete'
+    $grep = '! egrep -q'
   }
 
   exec { "semanage port ${port}, proto ${proto}, type ${name}":
-    command => "semanage port ${semanage} --type ${name} --proto ${proto} ${port}",
-    unless  => "semanage port --list | ( ${grep} '${re}' )", # subshell required to invert return status with !
+    command => "semanage port ${mgt} --type ${name} --proto ${proto} ${port}",
+    # subshell required to invert return status with !
+    unless  => "semanage port --list | ( ${grep} '${re}' )",
   }
-
 
 }
