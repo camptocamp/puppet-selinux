7209b7212ec697bf3fe12903a73be70a9e6a45ce
Christian Kaenzig <christian.kaenzig@camptocamp.com>
2014-01-16 17:10:53 +0100

    otrs: bump selinux module version

--- manifests/module.pp
+++ manifests/module.pp
@@ -60,16 +60,16 @@ define selinux::module (
   exec { "build selinux policy module ${name}":
     cwd     => $workdir,
     command => "checkmodule -M -m ${name}.te -o ${name}.mod",
-    creates => "${workdir}/${name}.mod",
+    onlyif  => "semodule -l | grep -q -P \"^${name}\t\"$(head -n1 ${name}.te | grep -o -e \"[0-9\.]*\")",
     require => [File["${workdir}/${name}.te"], Package['checkpolicy']],
     notify  => Exec["build selinux policy package ${name}"],
   }
 
   exec { "build selinux policy package ${name}":
-    cwd     => $workdir,
-    command => "semodule_package -o ${dest}/${name}.pp -m ${name}.mod",
-    creates => "${dest}/${name}.pp",
-    require => [
+    cwd         => $workdir,
+    command     => "semodule_package -o ${dest}/${name}.pp -m ${name}.mod",
+    refreshonly => true,
+    require     => [
       Exec["build selinux policy module ${name}"],
       Package['policycoreutils'],
     ],
