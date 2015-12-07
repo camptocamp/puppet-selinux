a08d131a60201d803c8bcc2d5fe12df3fce58d54
Mickaël Canévet <mickael.canevet@camptocamp.com>
2015-12-07 17:23:46 +0100

    Implement self.instances and self.prefetch

--- lib/puppet/provider/selinux_fcontext/semanage.rb
+++ lib/puppet/provider/selinux_fcontext/semanage.rb
@@ -5,9 +5,23 @@ Puppet::Type.type(:selinux_fcontext).provide(:semanage) do
   mk_resource_methods
 
   def self.instances
+    semanage('-n', '-l', '-C').split("\n").map do |fcontext|
+      name, *type, context = fcontext.split
+      new({
+        :ensure  => :present,
+        :name    => name,
+        :context => context,
+      })
+    end
   end
 
-  def self.prefetch
+  def self.prefetch(resources)
+    fcontexts = instances
+    resources.keys.each do |name|
+      if provider = fcontexts.find{ |fcontext| fcontext.name == name }
+        resources[name].provider = provider
+      end
+    end
   end
 
   def exists?
--- spec/unit/puppet/provider/selinux_fcontext/semanage_spec.rb
+++ spec/unit/puppet/provider/selinux_fcontext/semanage_spec.rb
@@ -22,6 +22,50 @@ describe Puppet::Type.type(:selinux_fcontext).provider(:semanage) do
           expect(described_class).to respond_to :prefetch
         end
       end
+
+      context 'without file contexts' do
+        before :each do
+          described_class.expects(:semanage).with('-n', '-l', '-C').returns ''
+        end
+        it 'should return no resources' do
+          expect(described_class.instances.size).to eq(0)
+        end
+      end
+
+      context 'with a file context' do
+        before :each do
+          described_class.expects(:semanage).with('-n', '-l', '-C').returns \
+            '/                                                  directory          system_u:object_r:root_t:s0 '
+        end
+        it 'should return one resource' do
+          expect(described_class.instances.size).to eq(1)
+        end
+        it 'should return / file' do
+          expect(described_class.instances[0].instance_variable_get("@property_hash")).to eq( {
+            :ensure    => :present,
+            :name      => '/',
+            :context   => 'system_u:object_r:root_t:s0',
+          } )
+        end
+      end
+
+      context 'with two file contexts' do
+        before :each do
+          described_class.expects(:semanage).with('-n', '-l', '-C').returns \
+            '/                                                  directory          system_u:object_r:root_t:s0 
+/.*                                                all files          system_u:object_r:default_t:s0 '
+        end
+        it 'should return two resources' do
+          expect(described_class.instances.size).to eq(2)
+        end
+        it 'should return /.*' do
+          expect(described_class.instances[1].instance_variable_get("@property_hash")).to eq( {
+            :ensure    => :present,
+            :name      => '/.*',
+            :context   => 'system_u:object_r:default_t:s0',
+          } )
+        end
+      end
     end
   end
 end
