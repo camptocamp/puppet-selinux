34420aee56fe5d4b5f8bcadadad3d9c062f3d325
Ugo Bellavance <ugob@lubik.ca>
2017-01-11 18:55:30 -0500

    Added support for wildcards in fcontexts. Also added an example.  This commit fixes the regex=>shell glob that wasn't working.

--- manifests/fcontext.pp
+++ manifests/fcontext.pp
@@ -23,6 +23,20 @@
 # ini the output of: semanage fcontext --list
 #/var/www(/.*)?                                     all files          system_u:object_r:httpd_sys_content_t:s0 # lint:ignore:80chars
 #
+# A more complex example:
+#
+#   # Make /var/www/*/*/logs httpd_log_t
+#   selinux::fcontext { '/var/www(/.*)(/.*)/logs':
+#    ensure      => present,
+#    recursive   => true,
+#    setype      => "httpd_log_t",
+#    refreshonly => true,
+#   }
+#
+# This will add a line to the configuration
+# It can be seen by executing semanage fcontext --list
+#/var/www(/.*)(/.*)/logs(/.*)?                      all files          system_u:object_r:httpd_log_t:s0
+
 define selinux::fcontext(
   $setype,
   $ensure    = 'present',
@@ -54,16 +68,16 @@ define selinux::fcontext(
       $grep     = '! egrep -q'
   }
 
-  $path_rc = regsubst( $path, '^(.*)\(.*', '\1' )
+# Change the regex into a shell glob
+  $pathrc = regsubst($path, '\(/\.\*\)', '/*', 'G')
 
   exec { "semanage fcontext ${setype} ${path}${path_glob}":
     path    => '/usr/bin:/usr/sbin:/bin:/sbin',
     command => "semanage fcontext -a -t ${setype} \"${path}${path_glob}\"",
     unless  => "semanage fcontext --list | ( ${grep} '${re}' >/dev/null)",
   } ~>
-  exec { "restorecon -R ${path}":
+  exec { "restorecon -R ${pathrc}":
     path        => '/usr/bin:/usr/sbin:/bin:/sbin',
-    command     => "restorecon -R ${path_rc}",
     refreshonly => $refreshonly,
   }
 
